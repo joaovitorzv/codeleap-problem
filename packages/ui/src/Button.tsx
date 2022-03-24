ab936f28c6b35c4c8fdf501a8819b78283f6fcfd
João V. Veras <joaovitorzv@outlook.com>
2022-03-24 19:35:04 -0300

    deploy: testing monorepo deploy on vercel

--- packages/ui/src/Button.tsx
+++ packages/ui/src/Button.tsx
@@ -1,5 +1,9 @@
 import * as React from "react";
 
-export const Button = ({ onClick }: { onClick: () => void }) => {
+interface Props {
+  onClick: () => void;
+}
+
+export const Button = ({ onClick }: Props) => {
   return <button onClick={onClick}>Boop</button>;
 };
